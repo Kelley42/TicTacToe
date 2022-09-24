aa15a16a8849216200438dc37ce29258975c1744
Kelley42 <kelleykristine42@yahoo.com>
2022-09-24 18:30:03 -0500

    Made grid squares clickable with x's

--- tictactoe.js
+++ tictactoe.js
@@ -9,3 +9,40 @@ Player.prototype.info = function() {
 
 const player1 = new Player("player 1", "X");
 const player2 = new Player("player 2", "O");
+
+function makeClickable() {
+    for (let i = 0; i < square.length; i++) {
+        square[i].addEventListener("click", () => {
+            if (square[i].innerHTML == "") {
+                square[i].innerHTML = "X";
+            }
+        });
+        // if (square[i].innerHTML == "") { // If box has not been taken yet
+        //     square[i].innerHTML = "X";
+        //     square[i].addEventListener("click", changeToFavColor);
+        //     break;
+        // }
+    }
+}
+
+
+const gameBoard = document.querySelector("#game-container");
+const square = document.getElementsByClassName("square");
+makeClickable();
+
+// Gameboard as array inside gameboard object
+// Players stored in objects
+// Object to control flow of gameplay
+
+// 1 of something - use module (gameBoard, displayController)
+// multiples - use factory (players)
+
+// JS function to render gameboard contents
+
+// Functions to allow players to mark spot, can't mark taken spots
+
+//Check for end of game - 3 in a row and tie (no 3 in row but no spaces left)
+
+// Players can input names
+// Start/restart game
+// Congratulate winner
